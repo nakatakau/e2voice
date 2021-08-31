d4778a7d8862fe0854d0af1805edb8a1855984fd
nakamura takuma <takuma.nakamura1131@gmail.com>
2021-08-31 19:55:11 +0900

    Initial Commit

---
+++ e2voice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  e2voice
+//
+//  Created by 中村拓磨 on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
